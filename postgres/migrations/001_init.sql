a2ea0929ec78eccd695307b90b0dc4698471da31
Tigeroff <parahinkv@gmail.com>
2025-04-27 17:01:05 +0300

    main

---
+++ postgres/migrations/001_init.sql
@@ -0,0 +1,59 @@
+\c logistic_db
+
+BEGIN;
+
+CREATE TYPE role AS ENUM ('user', 'admin');
+CREATE TYPE request_status AS ENUM ('created', 'accepted', 'followed', 'unfollowed', 'closed');
+CREATE TYPE segment_type AS ENUM ('initial', 'active');
+
+CREATE TABLE users (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    email text NOT NULL,
+    role role NOT NULL,
+    CONSTRAINT pk_users PRIMARY KEY (id)
+);
+
+CREATE TABLE requests (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    guid text NOT NULL,
+    user_id bigint NOT NULL,
+    status request_status NOT NULL,
+    start_width double precision NOT NULL,
+    start_height double precision NOT NULL,
+    end_width double precision NOT NULL,
+    end_height double precision NOT NULL,
+    creation_date timestamp with time zone NOT NULL,
+    last_update timestamp with time zone NOT NULL,
+    CONSTRAINT pk_requests PRIMARY KEY (id),
+    CONSTRAINT fk_requests_users_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE segments (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    request_id bigint NOT NULL,
+    type segment_type NOT NULL,
+    start_width double precision NOT NULL,
+    start_height double precision NOT NULL,
+    end_width double precision NOT NULL,
+    end_height double precision NOT NULL,
+    sequence_number integer NOT NULL,
+    length double precision NOT NULL,
+    CONSTRAINT pk_segments PRIMARY KEY (id),
+    CONSTRAINT fk_segments_requests_request_id FOREIGN KEY (request_id) REFERENCES requests (id) ON DELETE CASCADE
+);
+
+
+CREATE OR REPLACE FUNCTION update_last_update_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.last_update = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_last_update
+AFTER UPDATE ON requests
+FOR EACH ROW
+EXECUTE FUNCTION update_last_update_column();
+
+COMMIT;
