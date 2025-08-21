99309b1a3c09354869f3e6e93e9e28c671431ad3
Tigeroff <parahinkv@gmail.com>
2025-08-21 20:33:31 +0300

    some fixes. add setup for logistic-builder

--- postgres/migrations/001_init.sql
+++ postgres/migrations/001_init.sql
@@ -3,9 +3,10 @@
 BEGIN;
 
 CREATE TYPE role AS ENUM ('user', 'admin');
-CREATE TYPE request_status AS ENUM ('created', 'accepted', 'followed', 'unfollowed', 'closed');
+CREATE TYPE request_status AS ENUM ('created', 'calculated', 'accepted', 'followed', 'unfollowed', 'closed');
 CREATE TYPE request_type AS ENUM ('simple', 'travelling_salesman');
 CREATE TYPE segment_type AS ENUM ('initial', 'active');
+CREATE TYPE payment_status AS ENUM ('pending', 'waiting_for_capture', 'succeeded', 'canceled', 'authorized', 'partially_refunded', 'refunded', 'failed');
 
 CREATE TABLE users (
     id bigint GENERATED ALWAYS AS IDENTITY,
@@ -44,6 +45,35 @@ CREATE TABLE segments (
     CONSTRAINT fk_segments_requests_request_id FOREIGN KEY (request_id) REFERENCES requests (id) ON DELETE CASCADE
 );
 
+CREATE TABLE payments (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    guid text NOT NULL,
+    user_id bigint NOT NULL,
+    status payment_status NOT NULL,
+    amount decimal NOT NULL,
+    creation_date timestamp with time zone NOT NULL,
+    last_update timestamp with time zone NOT NULL,
+    CONSTRAINT pk_payments PRIMARY KEY (id),
+    CONSTRAINT fk_payments_users_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE subscriptions (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    user_id bigint NOT NULL,
+    payment_id bigint NOT NULL,
+    start_date timestamp with time zone NOT NULL,
+    end_date timestamp with time zone NOT NULL,
+    CONSTRAINT pk_subscriptions PRIMARY KEY (id),
+    CONSTRAINT fk_subscriptions_users_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_subscriptions_payments_payment_id FOREIGN KEY (payment_id) REFERENCES payments (id) ON DELETE CASCADE
+);
+
+CREATE TABLE outbox (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    data text NOT NULL,
+    timestamp bigint NOT NULL,
+    CONSTRAINT pk_outbox PRIMARY KEY (id)
+);
 
 CREATE OR REPLACE FUNCTION update_last_update_column()
 RETURNS TRIGGER AS $$
