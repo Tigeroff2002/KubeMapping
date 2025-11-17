63daf9fc38a04574770a0b099a9b7dbecd224a0a
Tigeroff <parahinkv@gmail.com>
2025-11-17 21:50:19 +0300

    syncronize migrations

--- postgres/migrations/001_init.sql
+++ postgres/migrations/001_init.sql
@@ -4,87 +4,46 @@ BEGIN;
 
 CREATE TYPE role AS ENUM ('user', 'admin');
 CREATE TYPE request_status AS ENUM ('created', 'calculated', 'accepted', 'followed', 'unfollowed', 'closed');
-CREATE TYPE request_type AS ENUM ('simple', 'travelling_salesman');
-CREATE TYPE segment_type AS ENUM ('initial', 'active');
+CREATE TYPE segment_type AS ENUM ('expected', 'overcomed');
 --CREATE TYPE payment_status AS ENUM ('pending', 'waiting_for_capture', 'succeeded', 'canceled', 'authorized', 'partially_refunded', 'refunded', 'failed');
 
 CREATE TABLE users (
     id bigint GENERATED ALWAYS AS IDENTITY,
     email text NOT NULL,
     role role NOT NULL,
-    CONSTRAINT pk_users PRIMARY KEY (id)
-);
+    CONSTRAINT pk_users PRIMARY KEY (id));
 
+-- builder domain
 CREATE TABLE requests (
     id bigint GENERATED ALWAYS AS IDENTITY,
     guid text NOT NULL,
     user_id bigint NOT NULL,
     status request_status NOT NULL,
-    request_type request_type NOT NULL DEFAULT 'simple',
     start_width double precision NOT NULL,
     start_height double precision NOT NULL,
-    end_width double precision NOT NULL,
-    end_height double precision NOT NULL,
     creation_date timestamp with time zone NOT NULL,
     last_update timestamp with time zone NOT NULL,
     CONSTRAINT pk_requests PRIMARY KEY (id),
-    CONSTRAINT fk_requests_users_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
+    CONSTRAINT fk_requests_users_user_id 
+        FOREIGN KEY (user_id) 
+        REFERENCES users (id) ON DELETE CASCADE);
 
-CREATE TABLE segments (
+CREATE TABLE requests_parts (
     id bigint GENERATED ALWAYS AS IDENTITY,
     request_id bigint NOT NULL,
-    type segment_type NOT NULL,
+    sequence_number smallint DEFAULT 1,
     start_width double precision NOT NULL,
     start_height double precision NOT NULL,
     end_width double precision NOT NULL,
     end_height double precision NOT NULL,
-    sequence_number integer NOT NULL,
-    length double precision NOT NULL,
-    CONSTRAINT pk_segments PRIMARY KEY (id),
-    CONSTRAINT fk_segments_requests_request_id FOREIGN KEY (request_id) REFERENCES requests (id) ON DELETE CASCADE
-);
-
-CREATE TABLE payments (
-    id bigint GENERATED ALWAYS AS IDENTITY,
-    guid text NOT NULL,
-    url text DEFAULT NULL,
-    user_id bigint NOT NULL,
-    status text NOT NULL,
-    amount decimal NOT NULL,
-    creation_date timestamp with time zone NOT NULL,
+    total_length double precision NOT NULL,
+    expected_duration interval NOT NULL,
+    actual_duration interval DEFAULT NULL,
     last_update timestamp with time zone NOT NULL,
-    CONSTRAINT pk_payments PRIMARY KEY (id),
-    CONSTRAINT fk_payments_users_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
-
-CREATE TABLE subscriptions (
-    id bigint GENERATED ALWAYS AS IDENTITY,
-    user_id bigint NOT NULL,
-    start_date timestamp with time zone NOT NULL,
-    end_date timestamp with time zone NOT NULL,
-    CONSTRAINT pk_subscriptions PRIMARY KEY (id),
-    CONSTRAINT fk_subscriptions_users_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
-
-CREATE TABLE request_commands_outbox (
-    id bigint GENERATED ALWAYS AS IDENTITY,
-    data text NOT NULL,
-    timestamp bigint NOT NULL,
-    CONSTRAINT pk_request_commands_outbox PRIMARY KEY (id)
-);
-
-CREATE TABLE payment_commands_outbox (
-    id bigint GENERATED ALWAYS AS IDENTITY,
-    data text NOT NULL,
-    timestamp bigint NOT NULL,
-    CONSTRAINT pk_payment_commands_outbox PRIMARY KEY (id)
-);
-
-CREATE TABLE user_balances(
-    user_id bigint NOT NULL PRIMARY KEY,
-    balance NUMERIC NOT NULL DEFAULT 0,
-    CONSTRAINT fk_users_balance_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE);
+    CONSTRAINT pk_requests_parts PRIMARY KEY(id),
+    CONSTRAINT fk_requests_parts_request_id 
+        FOREIGN KEY (request_id)
+        REFERENCES requests (id) ON DELETE CASCADE);
 
 CREATE OR REPLACE FUNCTION update_last_update_column()
 RETURNS TRIGGER AS $$
@@ -98,5 +57,64 @@ CREATE TRIGGER trigger_update_last_update
 AFTER UPDATE ON requests
 FOR EACH ROW
 EXECUTE FUNCTION update_last_update_column();
+
+CREATE TABLE segments (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    request_part_id bigint NOT NULL,
+    type segment_type NOT NULL,
+    start_width double precision NOT NULL,
+    start_height double precision NOT NULL,
+    end_width double precision NOT NULL,
+    end_height double precision NOT NULL,
+    sequence_number integer NOT NULL,
+    length double precision NOT NULL,
+    CONSTRAINT pk_segments PRIMARY KEY (id),
+    CONSTRAINT fk_segments_requests_parts_request_part_id 
+        FOREIGN KEY (request_part_id) 
+        REFERENCES requests_parts (id) ON DELETE CASCADE);
+
+-- payment domain
+CREATE TABLE payments (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    guid text NOT NULL,
+    url text DEFAULT NULL,
+    user_id bigint NOT NULL,
+    status text NOT NULL,
+    amount decimal NOT NULL,
+    creation_date timestamp with time zone NOT NULL,
+    last_update timestamp with time zone NOT NULL,
+    CONSTRAINT pk_payments PRIMARY KEY (id),
+    CONSTRAINT fk_payments_users_user_id 
+        FOREIGN KEY (user_id) 
+        REFERENCES users (id) ON DELETE CASCADE);
+
+CREATE TABLE user_balances(
+    user_id bigint NOT NULL PRIMARY KEY,
+    balance NUMERIC NOT NULL DEFAULT 0,
+    CONSTRAINT fk_users_balance_user_id 
+        FOREIGN KEY (user_id) 
+        REFERENCES users (id) ON DELETE CASCADE);
+
+CREATE TABLE subscriptions (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    user_id bigint NOT NULL,
+    start_date timestamp with time zone NOT NULL,
+    end_date timestamp with time zone NOT NULL,
+    CONSTRAINT pk_subscriptions PRIMARY KEY (id),
+    CONSTRAINT fk_subscriptions_users_user_id 
+        FOREIGN KEY (user_id) 
+        REFERENCES users (id) ON DELETE CASCADE);
+
+CREATE TABLE request_commands_outbox (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    data text NOT NULL,
+    timestamp bigint NOT NULL,
+    CONSTRAINT pk_request_commands_outbox PRIMARY KEY (id));
+
+CREATE TABLE payment_commands_outbox (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    data text NOT NULL,
+    timestamp bigint NOT NULL,
+    CONSTRAINT pk_payment_commands_outbox PRIMARY KEY (id));
 
 COMMIT;
--- postgres/migrations/002_additional_changes.sql
+++ postgres/migrations/002_additional_changes.sql
@@ -7,13 +7,29 @@ CREATE TYPE point_type AS ENUM ('start', 'end');
 CREATE TABLE path_points(
     id bigint GENERATED ALWAYS AS IDENTITY,
     user_id bigint NOT NULL,
-    point_type point_type NOT NULL,
+    request_id bigint NOT NULL,
     width double precision NOT NULL,
     height double precision NOT NULL,
-    is_classified boolean NOT NULL,
-    request_id bigint NOT NULL,
     CONSTRAINT pk_path_points PRIMARY KEY (id),
-    CONSTRAINT fk_path_points_request_id FOREIGN KEY (request_id) REFERENCES requests (id) ON DELETE CASCADE);
+    CONSTRAINT fk_path_points_user_id
+        FOREIGN KEY (user_id)
+        REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_path_points_request_id
+        FOREIGN KEY (request_id)
+        REFERENCES requests (id) ON DELETE CASCADE);
+
+CREATE TABLE requests_parts_path_points_entries(
+    point_id bigint NOT NULL,
+    request_part_id bigint NOT NULL,
+    point_type point_type NOT NULL,
+    CONSTRAINT pk_requests_parts_path_points_entries
+        PRIMARY KEY (point_id, request_part_id, point_type),
+    CONSTRAINT fk_requests_parts_path_points_entries_point_id
+        FOREIGN KEY (point_id)
+        REFERENCES path_points (id) ON DELETE CASCADE,
+    CONSTRAINT fk_requests_parts_path_points_entries_request_part_id
+        FOREIGN KEY (request_part_id)
+        REFERENCES requests_parts (id) ON DELETE CASCADE);
 
 CREATE TYPE day_of_week AS ENUM (
     'monday', 
@@ -24,20 +40,48 @@ CREATE TYPE day_of_week AS ENUM (
     'saturday', 
     'sunday');
 
+CREATE TYPE month_of_year AS ENUM (
+    'january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'june',
+    'july',
+    'august',
+    'september',
+    'october',
+    'november',
+    'december');
+
 CREATE TYPE area_type AS ENUM (
     'small', --area lesser than 30m
     'medium',  -- area on range [30, 100]m
     'complex'); -- area on range over 100m
 
-CREATE TABLE favorite_points(
+CREATE TABLE favorite_areas(
     id bigint GENERATED ALWAYS AS IDENTITY,
     user_id bigint NOT NULL,
     area_type area_type NOT NULL,
     width double precision NOT NULL,
     height double precision NOT NULL,
-    started_history bigint[] NOT NULL DEFAULT '{}',
-    end_history bigint[] NOT NULL DEFAULT '{}',
-    CONSTRAINT pk_favorite_points PRIMARY KEY (id));
+    CONSTRAINT pk_favorite_areas PRIMARY KEY (id),
+    CONSTRAINT fk_favorite_areas_user_id
+        FOREIGN KEY (user_id)
+        REFERENCES users (id) ON DELETE CASCADE);
+
+CREATE TABLE favorite_areas_points_entries(
+    point_id bigint NOT NULL,
+    fav_area_id bigint NOT NULL,
+    point_type point_type NOT NULL,
+    CONSTRAINT pk_favorite_areas_points_entries 
+        PRIMARY KEY (point_id, fav_area_id, point_type),
+    CONSTRAINT fk_favorite_areas_points_entries_point_id
+        FOREIGN KEY (point_id)
+        REFERENCES path_points(id) ON DELETE CASCADE,
+    CONSTRAINT fk_favorite_areas_points_entries_fav_area_id
+        FOREIGN KEY (fav_area_id)
+        REFERENCES favorite_areas(id) ON DELETE CASCADE);
 
 CREATE TABLE time_of_day_directory(
     id bigint GENERATED ALWAYS AS IDENTITY,
@@ -54,10 +98,24 @@ INSERT INTO time_of_day_directory (start_range, end_range) VALUES ('16 hours 30 
 INSERT INTO time_of_day_directory (start_range, end_range) VALUES ('19 hours'::interval, '22 hours'::interval);
 INSERT INTO time_of_day_directory (start_range, end_range) VALUES ('22 hours'::interval, '1 hours'::interval);
 
+CREATE TABLE cities_directory(
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    center_width double precision NOT NULL,
+    center_heigth double precision NOT NULL,
+    radius double precision NOT NULL,
+    CONSTRAINT pk_cities_directory PRIMARY KEY(id));
+
+INSERT INTO cities_directory(name, center_width, center_heigth, radius) 
+    VALUES ('Vladimir', 56.140986, 40.404883, 6850);
+
 CREATE TABLE finalized_requests(
     id bigint GENERATED ALWAYS AS IDENTITY,
     user_id bigint NOT NULL,
     request_id bigint NOT NULL,
+    total_length double precision NOT NULL,
+    cities_directory_id bigint NOT NULL,
+    month_of_year month_of_year NOT NULL,
     time_of_day_directory_id bigint NOT NULL,
     day_of_week day_of_week NOT NULL,
     start_point_id bigint NOT NULL,
@@ -65,13 +123,51 @@ CREATE TABLE finalized_requests(
     expected_duration interval NOT NULL,
     actual_duration interval NOT NULL,
     CONSTRAINT pk_finalized_requests PRIMARY KEY (id),
-    CONSTRAINT fk_finalized_requests_request_id FOREIGN KEY (request_id) REFERENCES requests (id) ON DELETE CASCADE,
-    CONSTRAINT fk_finalized_requests_time_of_day_directory_id FOREIGN KEY (time_of_day_directory_id) REFERENCES time_of_day_directory (id));
+    CONSTRAINT fk_finalized_requests_user_id
+        FOREIGN KEY (user_id)
+        REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_finalized_requests_request_id
+        FOREIGN KEY (request_id) 
+        REFERENCES requests (id) ON DELETE CASCADE,
+    CONSTRAINT fk_finalized_requests_time_of_day_directory_id 
+        FOREIGN KEY (time_of_day_directory_id)
+        REFERENCES time_of_day_directory (id),
+    CONSTRAINT fk_finalized_requests_cities_directory_id
+        FOREIGN KEY (cities_directory_id)
+        REFERENCES cities_directory (id));
 
 CREATE TABLE request_commands_v2_outbox (
     id bigint GENERATED ALWAYS AS IDENTITY,
     data text NOT NULL,
     timestamp bigint NOT NULL,
     CONSTRAINT pk_request_commands_v2_outbox PRIMARY KEY (id));
+
+CREATE TABLE users_history_directory (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    user_id bigint NOT NULL,
+    cities_directory_id bigint NOT NULL,
+    start_fav_area_id bigint NOT NULL,
+    end_fav_area_id bigint NOT NULL,
+    month_of_year month_of_year NOT NULL,
+    time_of_day_directory_id bigint NOT NULL,
+    day_of_week day_of_week NOT NULL,
+    number_of_rides smallint NOT NULL,
+    average_duration interval NOT NULL,
+    CONSTRAINT pk_users_history_directory PRIMARY KEY (id),
+    CONSTRAINT fk_users_history_directory
+        FOREIGN KEY (user_id)
+        REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_users_history_directory_cities_directory_id
+        FOREIGN KEY (cities_directory_id)
+        REFERENCES cities_directory (id),
+    CONSTRAINT fk_users_history_directory_start_fav_area_id 
+        FOREIGN KEY (start_fav_area_id) 
+        REFERENCES favorite_areas (id) ON DELETE CASCADE,
+    CONSTRAINT fk_users_history_directory_end_fav_area_id 
+        FOREIGN KEY (end_fav_area_id) 
+        REFERENCES favorite_areas (id) ON DELETE CASCADE,
+    CONSTRAINT fk_users_history_directory_time_of_day_directory_id 
+        FOREIGN KEY (time_of_day_directory_id)
+        REFERENCES time_of_day_directory (id));
 
 COMMIT;
